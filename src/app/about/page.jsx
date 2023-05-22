c7dd1987098c48093832a85253fd4e17af972481
Lucas <luccaslp99@gmail.com>
2023-05-22 11:00:06 -0300

    create page about

---
+++ src/app/about/page.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum odio risus, venenatis et bibendum condimentum, posuere et nunc. Pellentesque molestie volutpat sapien nec lobortis. Suspendisse in arcu vitae nunc viverra dictum. Donec eget tincidunt ipsum, nec ullamcorper risus. Aliquam eu efficitur enim. Maecenas vel ligula quis eros sodales faucibus. Etiam vitae metus lacus. Duis mattis dictum tristique. Donec quis iaculis nisi. Fusce porta ipsum vel eros suscipit sagittis. Maecenas vitae mi venenatis, vestibulum tortor a, tincidunt sapien.
+      </p>
+
+      <p>
+      Praesent suscipit iaculis ex eu ullamcorper. Etiam ac dui ut ex molestie placerat. Donec finibus, est in commodo fringilla, urna risus eleifend leo, vitae scelerisque velit urna quis magna. Nunc ullamcorper, arcu et euismod rhoncus, arcu justo fermentum mauris, ut euismod odio sapien eu enim. Vestibulum viverra tortor lorem, pulvinar porttitor libero tristique quis. Phasellus at finibus sem. Morbi sed ultricies purus. Mauris cursus pulvinar nulla. Nam consectetur quis leo ut blandit. Praesent in augue eu leo dignissim rutrum. Cras fringilla sem id felis pharetra euismod. Fusce elementum dictum ultrices.
+      </p>
+
+      <p>
+      Nullam pellentesque venenatis mauris, non euismod sem elementum ac. Nulla tristique magna cursus dapibus efficitur. Maecenas sollicitudin justo vitae velit ultrices, sed consequat libero laoreet. Sed vitae libero ac justo consequat aliquam. In ultrices turpis a commodo tincidunt. Nam eleifend, odio vel rutrum tincidunt, ligula lorem pellentesque diam, a faucibus nisi enim at tortor. Praesent ipsum purus, facilisis a aliquet vitae, varius vel sem. In suscipit ligula et congue sollicitudin. Sed consequat nec ligula sit amet facilisis. Integer auctor a eros sed pharetra. In sagittis pharetra nibh id suscipit. Pellentesque nec pellentesque dui, sit amet molestie metus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean at rhoncus velit.
+      </p>
+    </div>
+  )
+}
