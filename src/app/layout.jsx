c7774b0fadb09f2a4b4f7f000a4356335e1b4627
Lucas <luccaslp99@gmail.com>
2023-05-22 13:45:11 -0300

    create the navbar component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,8 @@
 import './globals.css'
 
-import { Header } from '@/components/Header'
 import { Providers } from '@/app/Providers'
+import { Header } from '@/components/Header'
+import { Navbar } from '@/components/Navbar'
 
 export const metadata = {
   title: 'IMDB',
@@ -14,7 +15,9 @@ export default function RootLayout({ children }) {
       <body >
         <Providers>
           <Header />
+          <Navbar />
           {children}
+
         </Providers>
       </body>
     </html>
